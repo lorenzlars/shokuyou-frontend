e462d18fe4ebd00addedc2705c9fce9af147ed16
Lars Lorenz <mail@larslorenz.dev>
2024-12-20 19:46:41 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+console.log(process.env)
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
