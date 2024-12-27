4fab1140b0311b901530d1a60bea44ec5ad6d5f0
Lars Lorenz <mail@larslorenz.dev>
2024-12-27 14:17:12 +0100

    hosting test

---
+++ docs/heroku.md
@@ -0,0 +1,7 @@
+# Heroku
+
+## Frontend
+
+### Hosting setup
+https://devcenter.heroku.com/articles/deploying-php
+https://devcenter.heroku.com/articles/custom-php-settings
--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,10 @@ import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'node:path'
-import * as fs from 'node:fs'
-
-// Needed for Heroku PHP
-function renameIndexPlugin(fileName?: string) {
-  return {
-    name: 'rename-index-plugin',
-    closeBundle() {
-      const distPath = path.resolve(__dirname, 'dist')
-      const indexHtmlPath = path.join(distPath, 'index.html')
-      const indexPhpPath = path.join(distPath, fileName ?? 'index.php')
-
-      if (fs.existsSync(indexHtmlPath)) {
-        fs.renameSync(indexHtmlPath, indexPhpPath)
-        console.log('index.html wurde zu index.php umbenannt!')
-      }
-    },
-  }
-}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), UnoCSS(), renameIndexPlugin()],
+  plugins: [vue(), vueDevTools(), UnoCSS()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
