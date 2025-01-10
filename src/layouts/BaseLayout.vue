d7b5ccff1cfe10a58b74b085683afff70b5ab707
Lars Lorenz <mail@larslorenz.dev>
2025-01-10 11:25:58 +0100

    z index fix

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -38,7 +38,7 @@ function handleLogout() {
 
 <template>
   <header
-    class="bg-light flex justify-between items-center fixed top-0 left-0 right-0 shadow-lg h-18"
+    class="bg-light flex justify-between items-center fixed top-0 left-0 right-0 shadow-lg h-18 z-1000"
   >
     <div class="container flex items-center gap-12">
       <RouterLink class="text-6 font-black no-underline text-text hover:no-underline w-fit" to="/">
